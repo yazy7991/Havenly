9e988feb3a685302f318ac7b313707c569ec0d71
yazy7991 <jj3369155@gmail.com>
2025-08-03 19:32:08 +0100

    The initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+ 
+  return (
+    <>
+      <h1>
+        This is the heading
+      </h1>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {asset} from '../assets/assets';
+
+function Navbar() {
+  return (
+    <div className='absolute top-0 left-0 w-full z-10'>
+        <img src={asset} alt=''/>
+      
+    </div>
+  )
+}
+
+export default Navbar
