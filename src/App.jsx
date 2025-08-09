0fc66f255acea07f3d3b277fe3c34515a524a3eb
yazy7991 <jj3369155@gmail.com>
2025-08-09 21:37:13 +0100

    Added Contact Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <About />
       <Projects />
       <Testimonials />
+      <Contact />
       
     </div>
   )
---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' 
+    id='Contact'>
+        <h1 className='text-2xl sm:text-4xl
+        font-bold mb-2 text-center'>Contact<span className='underline underline-offset-4 
+        decoration-1 under font-light'>With Us</span></h1>
+        <p className='text-center text-gray-500
+        mb-12 max-w-80 mx-auto'>
+            Ready to Make a Move? Let's Build Your Future Together
+        </p>
+
+        <for</for>
+      
+    </div>
+  )
+}
+
+export default Contact
