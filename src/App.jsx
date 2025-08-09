687419a889db704ab0d5dd1d95381d9d5b061aef
yazy7991 <jj3369155@gmail.com>
2025-08-09 19:10:37 +0100

    Added Testimonials Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <About />
       <Projects />
+      <Testimonials />
       
     </div>
   )
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div>
+        <h1>Customer<span>Testimonials</span></h1>
+        <p>
+            Real Stories from Those Who Found Home with Us
+        </p>
+      
+    </div>
+  )
+}
+
+export default Testimonials
