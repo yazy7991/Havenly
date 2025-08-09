f09c81a23a1063ca0276ef92b321891fd2c037cf
yazy7991 <jj3369155@gmail.com>
2025-08-09 22:03:35 +0100

    Added Form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,35 @@ const Contact = () => {
             Ready to Make a Move? Let's Build Your Future Together
         </p>
 
-        <for</for>
+        <form className='max-w-2xl mx-auto text-gray-600 pt-8'>
+            <div className='flex flex-wrap'>
+                <div className='w-full md:w-1/2 text-left'>
+                    Your Name
+                    <input className='w-full border border-gray-300
+                    rounded py-3 px-4 mt-2'
+                    type='text' name='Name' placeholder='Your Name' required/>
+                </div>
+                <div className='w-full md:w-1/2 text-left md:pl-4'>
+                    Your Email
+                    <input className='w-full border border-gray-300
+                    rounded py-3 px-4 mt-2'
+                    type='email' name='Email' placeholder='Your Email' required/>
+                </div>
+            </div>
+            <div className='my-6 text-left'>
+                Message 
+                <textarea className='w-full border border-gray-300
+                rounded py-3 px-4 mt-2 h-48 resize-none'
+                name='Message' placeholder='Message' required>
+
+                </textarea>
+
+            </div>
+
+            <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>
+                Send Message
+            </button>
+        </form>
       
     </div>
   )
