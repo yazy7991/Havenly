eeae33b2f6cd45ea5f7361822ece22bd2a383e04
yazy7991 <jj3369155@gmail.com>
2025-08-10 13:57:53 +0100

    Newsletter subscribe button added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,7 +29,27 @@ const Footer = () => {
                     className='hover:text-white'>Privacy policy</a>
                 </ul>
             </div>
-            <div></div>
+            <div className='w-full md:w-1/3'>
+                <h3 className='text-white text-lg font-bold mb-4'>
+                    Subscribe to our newsletter
+                </h3>
+                <p className='text-gray-400 mb-4 max-w-80'>
+                    The latest news, articles, and resources, sent to your inbox weekly.
+                </p>
+                <div className='flex gap-2'>
+                    <input 
+                    className='p-2 rounded bg-gray-800 text-gray-400
+                    border border-gray-700 focus:outline-none w-full
+                    md:w-auto'
+                    type='email' placeholder='Enter your email'/>
+                    <button
+                    className='py-2 px-4 rounded bg-blue-500 
+                    text-white'>Subscribe</button>
+                </div>
+                
+
+
+            </div>
         </div>
         <div></div>
       
