ff8a9a85a5edaa21ff0ae0b49bafab009e35aaf8
yazy7991 <jj3369155@gmail.com>
2025-08-10 12:14:05 +0100

    Web3Forms added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,35 @@
 import React from 'react'
 
 const Contact = () => {
+    const [result, setResult] = React.useState("");
+    
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", import.meta.env.VITE_ACCESS_KEY);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      alert("Form Submitted Successfully")
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      alert(data.message)
+      setResult("");
+    }
+  };
+
+
   return (
     <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' 
     id='Contact'>
@@ -12,7 +41,8 @@ const Contact = () => {
             Ready to Make a Move? Let's Build Your Future Together
         </p>
 
-        <form className='max-w-2xl mx-auto text-gray-600 pt-8'>
+        <form onSubmit={onSubmit}
+        className='max-w-2xl mx-auto text-gray-600 pt-8'>
             <div className='flex flex-wrap'>
                 <div className='w-full md:w-1/2 text-left'>
                     Your Name
@@ -38,7 +68,7 @@ const Contact = () => {
             </div>
 
             <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>
-                Send Message
+                {result?result:"Send Message"}
             </button>
         </form>
       
