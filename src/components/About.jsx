1ad5a18cb9cea875622e159a1671af491c331ea4
yazy7991 <jj3369155@gmail.com>
2025-08-10 14:26:51 +0100

    Animated the About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { motion } from "motion/react" 
 
 export default function About() {
   return (
-    <div className='flex flex-col items-center justify-center
+    <motion.div 
+
+    initial={{opacity:0, x:200}}
+    transition={{duration:1}}
+    whileInView={{opacity:1,x:0}}
+    viewport={{once:true}}
+    
+    className='flex flex-col items-center justify-center
     container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden'
     id='About'>
         <h1 className='text-2xl sm:text-4xl font-bold mb-2
@@ -50,6 +58,6 @@ export default function About() {
 
             </div>
         </div>     
-    </div>
+    </motion.div>
   )
 }
